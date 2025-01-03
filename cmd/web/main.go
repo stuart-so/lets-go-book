0ba8dfb0028166a8cb2d5bfa8fd8e77168639e65
stuart-so <94648423+stuart-so@users.noreply.github.com>
2025-01-03 15:02:47 -0700

    pro

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -8,11 +9,15 @@ import (
 	"net/http"
 	"os"
 
+	"snippetbox.stuarternstsen.com/internal/models"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
 
 type application struct {
 	logger *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func getENVValue(key string, defaultValue any)(string){
@@ -33,18 +38,46 @@ func getENVValue(key string, defaultValue any)(string){
 func main() {
 	//Application config setup
 	envDefault_addr := getENVValue("addr", "localhost:4000")
+	envDefault_dsn := getENVValue("dsn", nil)
 	addr := flag.String("addr", envDefault_addr, "HTTP network address")
+	dsn := flag.String("dsn", envDefault_dsn, "MySQL data source name")
 	flag.Parse()
-	
-	//Application shared dependencies
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	app := &application{
-		logger: logger,
+	
+
+
+	//DB setup
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
 	}
 	
+	defer db.Close()
+
+	//Application shared dependencies
+	app := &application{
+		logger: logger,
+		snippets: &models.SnippetModel{DB: db},
+	}
+
 	//Run the server
 	logger.Info("starting server", slog.String("addr", *addr))
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+// for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	} 
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	} 
+	return db, nil
 }
---
+++ internal/models/snippets.go
@@ -0,0 +1,62 @@
+package models
+
+import (
+	"database/sql"
+	"errors"
+	"time"
+)
+
+type Snippet struct {
+	ID int
+	Title string
+	Content string
+	Created time.Time
+	Expires time.Time
+}
+
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error){
+	query := `INSERT INTO snippets (title, content, created, expires)
+	values(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(query, title, content, expires)
+
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId();
+
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
+
+func (m *SnippetModel) Get(id int) (Snippet, error) {
+	query := `SELECT * from snippets
+	where expires > UTC_TIMESTAMP() AND id = ?`
+
+	row := m.DB.QueryRow(query, id)
+
+	var s Snippet
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows){
+			return Snippet{}, ErrNoRecord
+		} else {
+			return Snippet{}, err
+		}
+	}
+
+	return s, nil
+}
+
+func (m *SnippetModel) Latest() ([]Snippet, error) {
+	return nil, nil
+}
